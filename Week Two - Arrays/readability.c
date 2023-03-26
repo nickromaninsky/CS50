cc716c7e8383f9b5f463e1476ba2114a38888623
nickromaninsky <nickromo28@gmail.com>
2023-03-26 01:26:20 -0400

    Added week two.

---
+++ Week Two - Arrays/readability.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>//f
+#include <math.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    int letterscount = 0;//f
+    int wordcount = 1;
+    int sentencecount = 0;
+
+//count words
+    for (int i = 0; i < strlen(text); i++)
+    {
+       if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+       {
+           letterscount++;
+       }
+       else if (text[i] == ' ')
+       {
+           wordcount++;
+       }
+       else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+       {
+           sentencecount++;//hi
+       }
+    }
+    float grade = 0.0588 * (100 * (float) letterscount / (float) wordcount) - 0.296 * (100 * (float) sentencecount / (float) wordcount) - 15.8;
+    if (grade < 16 && grade >= 0)
+    {
+        printf("Grade %i\n", (int) round(grade));
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else//f
+    {
+        printf("Before Grade 1\n");
+    }//g
+
+}//f
